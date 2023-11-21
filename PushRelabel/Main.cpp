5333b5e087769eac3e74485a883586f6a9927a01
= <stefa.manescotto@gmail.com>
2023-11-21 09:50:30 +0100

    Add parallel push relabel

--- PushRelabel/Main.cpp
+++ PushRelabel/Main.cpp
@@ -5,13 +5,37 @@
 #include <unordered_map>
 #include "graph.hpp"
 
-int main() {
-	graph g = graph();
-	std::unordered_map<int, node *> node_map;
+#include <chrono>
 
+void create_graph_file();
+graph get_graph();
+
+std::unordered_map<int, node*> node_map;
+
+int main() {
+
+	//create_graph_file();
+
+	graph g = get_graph();
+		
+	auto start = chrono::high_resolution_clock::now();
+
+	//int flux = g.get_max_flow(*node_map[1], *node_map[4]);
+	int flux = g.get_max_flow_parallel(*node_map[1], *node_map[4]);
+
+	auto stop = chrono::high_resolution_clock::now();
+	auto duration = chrono::duration_cast<chrono::microseconds> (stop - start);
+
+	cout << "max flux: " << flux << " - time: " << duration.count() << "\n";
+
+	return 0;
+}
+
+graph get_graph() {
+	graph g = graph();
 	string line;
 	std::ifstream file;
-	file.open("graph_generator.txt");
+	file.open("graph1.txt");
 
 	if (file.is_open()) {
 		while (getline(file, line)) {
@@ -24,23 +48,55 @@ int main() {
 			if (node_map.find(u_id) == node_map.end()) {
 				node* u = new node(u_id);
 				g.add_node(u);
-				node_map.insert({u_id, u});
+				node_map.insert({ u_id, u });
 			}
 
 			if (node_map.find(v_id) == node_map.end()) {
-				node *v = new node(v_id);
+				node* v = new node(v_id);
 				g.add_node(v);
-				node_map.insert({v_id, v});
+				node_map.insert({ v_id, v });
 			}
 
 			g.add_edge(*node_map[u_id], *node_map[v_id], stoi(s_capacity));
 
 		}
-
-		int flux = g.get_max_flow(*node_map[1], *node_map[4]);
-		cout << "max flux: " << flux;
-
-		return 0;
 	}
+
+	return g;
 }
 
+// create a random graph and saves it into a file ("graph1.txt")
+void create_graph_file() {
+	int n_nodes = 500; // number of nodes of the random graph
+
+	vector<int> node_neighbors;
+	ofstream myfile;
+	myfile.open("graph1.txt");
+
+	srand((unsigned)time(NULL));
+
+	for (int i = 1; i <= n_nodes; i++) {
+
+		int n = 1 + rand() % 10; // number of children
+
+		for (int k = 0; k < n; k++) {
+			string new_line = "";
+
+			int n2 = 1 + rand() % n_nodes;
+			while (n2 == i || find(node_neighbors.begin(), node_neighbors.end(), n2) != node_neighbors.end()) {
+				n2 = 1 + rand() % n_nodes;
+			}
+
+			node_neighbors.push_back(n2);
+			int n3 = 5 + rand() % 20; // capacity of new edge
+
+			new_line += to_string(i) + " " + to_string(n2) + " " + to_string(n3);
+			new_line += "\n";
+
+			myfile << new_line;
+		}
+		node_neighbors = vector<int>();
+	}
+
+	myfile.close();
+}
--- PushRelabel/graph.hpp
+++ PushRelabel/graph.hpp
@@ -1,22 +1,30 @@
-#include <cassert>
 #include <vector>
+#include <omp.h>
 
 using namespace std;
+
+struct edge;
 
 struct node {
 	int id;
 	int height;
 	int e_flow; //excess flow
+	vector<edge *> neighbors;
+	omp_lock_t writelock;
 
-	node() : id(0), height(0), e_flow(0){}
+	node() : id(0), height(0), e_flow(0){
+		omp_init_lock(&writelock);
+	}
 
 	node(int id) {
+		omp_init_lock(&writelock);
 		this->id = id;
 		this->height = 0;
 		this->e_flow = 0;
 	}
 
 	node(int id, int height, int e_flow) {
+		omp_init_lock(&writelock);
 		this->id = id;
 		this->height = height;
 		this->e_flow = e_flow;
@@ -28,27 +36,36 @@ struct edge {
 	node *v;
 	int capacity;
 	int flow;
+	omp_lock_t writelock;
 
 	edge(node *u, node *v, int capacity, int flow = 0) {
 		this->capacity = capacity;
 		this->flow = flow;
 		this->u = u;
 		this->v = v;
+		omp_set_lock(&u->writelock);
+		u->neighbors.push_back(this);
+		omp_unset_lock(&u->writelock);
 	}
 };
 
 class graph {
 	vector<node *> nodes;
-	vector<edge> edges;
+	vector<edge *> edges;
+	omp_lock_t g_writelock;
 
 public:
+
+	graph() {
+		omp_init_lock(&g_writelock);
+	}
+
 	void add_node(node *new_node) {
 		nodes.push_back(new_node);
 	}
 
 	void add_edge(node& u, node& v, int capacity) {
-		edge new_edge = edge(&u, &v, capacity);
-		edges.push_back(new_edge);
+		edges.push_back(new edge(&u, &v, capacity));
 	}
 
 
@@ -62,30 +79,56 @@ public:
 			}
 		}*/
 
-
 		while ((over_flow_node = get_over_flow_node(s, t)) != nullptr) {
 			if (over_flow_node != &s && over_flow_node != &t) {
 				discharge(*over_flow_node);
 			}
 		}
 
+		return t.e_flow;
+	}
+
+
+	int get_max_flow_parallel(node& s, node& t) {;
+		preflow(s);
+		node* over_flow_node;
+
+		#pragma omp parallel
+		{
+			#pragma omp single
+			{
+				vector<node *> over_nodes = get_over_flow_nodes(s, t);
+				while (over_nodes.size() > 0) {
+					for (node *n : over_nodes) {
+						#pragma omp task
+						discharge(*n);
+					}
+					#pragma omp taskwait
+					over_nodes = get_over_flow_nodes(s, t);
+				}
+			}
+		}
 
 		return t.e_flow;
 	}
 
 private:
 	void preflow(node& source) {
-		source.height = static_cast<int>(nodes.size());
+		vector<edge *> new_edges;
 
-		for (edge& e : edges) {
-			if (e.u == &source) {
-				e.flow = e.capacity;
-				e.v->e_flow += e.flow;
-				edges.push_back(edge(e.v, e.u, 0, -e.flow));
+		source.height = static_cast<int>(nodes.size());
+		for (edge *e : edges) {
+			if (e->u == &source) {
+				e->flow = e->capacity;
+				e->v->e_flow += e->flow;
+
+				new_edges.push_back(new edge(e->v, e->u, 0, -e->flow));
 			}
 		}
+		edges.insert(edges.end(), new_edges.begin(), new_edges.end());
 	}
 
+	//return first node that has excess flow
 	node* get_over_flow_node(node& source, node& t) {
 		for (node* n : nodes) {
 			if (&source != n && n != &t && n->e_flow > 0) {
@@ -95,30 +138,45 @@ private:
 		return nullptr;
 	}
 
+	//return vector containing all nodes that have excess flow
+	vector<node *> get_over_flow_nodes(node& source, node& t) {
+		vector<node *> over_nodes;
+		for (node* n : nodes) {
+			if (&source != n && n != &t && n->e_flow > 0) {
+				over_nodes.push_back(n);
+			}
+		}
+		return over_nodes;
+	}
+
 	void reverse_edge_flow(edge& e_param, int flow) {
-		for (edge& e : edges) {
-			if (e_param.u == e.v && e_param.v == e.u) {
-				e.flow -= flow;
+		omp_set_lock(&e_param.v->writelock);
+		for (edge *e : e_param.v->neighbors) {
+			if (e_param.u == e->v) {
+				e->flow -= flow;
+				omp_unset_lock(&e_param.v->writelock);
 				return;
 			}
 		}
+		omp_unset_lock(&e_param.v->writelock);
 
 		// reverse edge does not exist: add it
-		edge a = edge(e_param.v, e_param.u, 0, -flow);
-		edges.push_back(a);
+		omp_set_lock(&g_writelock);
+		edges.push_back(new edge(e_param.v, e_param.u, 0, -flow));
+		omp_unset_lock(&g_writelock);
 	}
 
 	bool push(node& u) {
-		for (edge& e : edges) {
-			if (e.u == &u) {
-				if ((u.height > e.v->height) && (e.flow != e.capacity)) {
-					int flow = min(e.capacity - e.flow, u.e_flow);
+		for (edge *e : edges) {
+			if (e->u == &u) {
+				if ((u.height > e->v->height) && (e->flow != e->capacity)) {
+					int flow = min(e->capacity - e->flow, u.e_flow);
 
 					u.e_flow -= flow;
-					e.v->e_flow += flow;
-					e.flow += flow;
+					e->v->e_flow += flow;
+					e->flow += flow;
 
-					reverse_edge_flow(e, flow);
+					reverse_edge_flow(*e, flow);
 
 					return true;
 				}
@@ -129,41 +187,52 @@ private:
 
 	void relabel(node& u) {
 		int max_height = INT_MAX;
-		for (edge& e : edges) {
-			if (e.u == &u) {
-				if (e.flow == e.capacity) {
+		omp_set_lock(&u.writelock);
+		for (edge *e : u.neighbors) {
+			node* test = e->u;
+			if (e->u == &u) {
+				if (e->flow == e->capacity) {
 					continue;
 				}
 				
-				if (e.v->height < max_height) {
-					max_height = e.v->height;
+				if (e->v->height < max_height) {
+					max_height = e->v->height;
 					u.height = max_height + 1;
 				}
 			}
 		}
+		omp_unset_lock(&u.writelock);
 	}
 
 	void push2(edge& e) {
 		int flow = min(e.capacity - e.flow, e.u->e_flow);
 
 		e.u->e_flow -= flow;
+		omp_unset_lock(&e.u->writelock);
+		
+		omp_set_lock(&e.v->writelock);
 		e.v->e_flow += flow;
+		omp_unset_lock(&e.v->writelock);
+
 		e.flow += flow;
 
 		reverse_edge_flow(e, flow);
+		omp_set_lock(&e.u->writelock);
 	}
 
 	void discharge(node& u) {
 		int curr_edge = 0;
 		while (u.e_flow > 0) {
-			if (curr_edge >= edges.size()) {
+			if (curr_edge >= u.neighbors.size()) {
 				relabel(u);
 				curr_edge = 0;
 			}
 			else {
-				if (edges.at(curr_edge).u->height == edges.at(curr_edge).v->height + 1) {
-					push2(edges.at(curr_edge));
+				omp_set_lock(&u.writelock);
+				if (u.height == u.neighbors.at(curr_edge)->v->height + 1) {
+					push2(*u.neighbors.at(curr_edge));
 				}
+				omp_unset_lock(&u.writelock);
 				curr_edge++;
 			}
 		}
